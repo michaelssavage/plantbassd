4fe4688176472b2741638f20381d14bd873b8df7
savagem7 <i0g1n4oj>
2021-12-19 20:44:15 +0000

    async contact form with toast messages added.

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Button, Form, InputGroup, Spinner } from "react-bootstrap";
+import { Form, InputGroup, Spinner } from "react-bootstrap";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function ContactForm() {
 	const [formData, setFormData] = useState({});
@@ -11,123 +12,124 @@ export default function ContactForm() {
 		});
 	};
 
-	const handleSubmit = (e) => {
+	async function handleSubmit(e) {
 		e.preventDefault();
 		setLoading(true);
 		console.log("Sending");
+
 		try {
-			fetch("/api/contact", {
-				method: "POST",
+			const response = await fetch("/api/contact", {
+				method: "post",
 				headers: {
 					Accept: "application/json, text/plain, */*",
 					"Content-Type": "application/json",
 				},
 				body: JSON.stringify(formData),
-			}).then((res) => {
-				console.log("Response received");
-
-				if (res.status === 200) {
-					console.log("Response succeeded!");
-					setFormData({
-						name: formData.name,
-						email: "",
-						message: "",
-					});
-				} else {
-					throw new Error(res);
-				}
 			});
-		} catch (err) {
-			console.log(err);
+
+			const res = await response.json();
+			console.log(res);
+
+			if (res.error) {
+				toast.error("Form didn't submit!");
+				throw new Error(res.error);
+			}
+			toast.success("Thanks, we'll be in touch!");
+			setFormData({
+				name: "",
+				email: "",
+				message: "",
+			});
+		} catch (error) {
+			toast.error(error.message);
+		} finally {
+			setLoading(false);
 		}
-		setLoading(false);
-	};
+	}
 
 	return (
-		<Form
-			onSubmit={(e) => {
-				handleSubmit(e);
-			}}
-		>
-			<Form.Group column md="6" controlId="validationFName">
-				<Form.Label>Name</Form.Label>
-				<Form.Control
-					name="name"
-					required
-					type="text"
-					placeholder="Enter name"
-					onChange={updateInput}
-					value={formData.name || ""}
-				/>
-				<Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-				<Form.Control.Feedback type="invalid">
-					Please enter your name.
-				</Form.Control.Feedback>
-			</Form.Group>
-
-			<Form.Group column md="6" controlId="formEmail">
-				<Form.Label>Email Address</Form.Label>
-				<InputGroup>
+		<>
+			<Form
+				onSubmit={(e) => {
+					handleSubmit(e);
+				}}
+			>
+				<Form.Group controlId="validationFName">
+					<Form.Label>Name</Form.Label>
 					<Form.Control
-						name="email"
-						type="email"
-						placeholder="Enter email"
+						name="name"
 						required
+						type="text"
+						placeholder="Enter name"
 						onChange={updateInput}
-						value={formData.email || ""}
+						value={formData.name || ""}
 					/>
 					<Form.Control.Feedback>Looks good!</Form.Control.Feedback>
 					<Form.Control.Feedback type="invalid">
-						Please enter a valid email.
+						Please enter your name.
 					</Form.Control.Feedback>
-				</InputGroup>
-			</Form.Group>
+				</Form.Group>
 
-			<Form.Group column controlId="contactForm.Textarea">
-				<Form.Label>Message</Form.Label>
-				<Form.Control
-					as="textarea"
-					rows={3}
-					placeholder="Please enter your message"
-					required
-					name="message"
-					onChange={updateInput}
-					value={formData.message || ""}
-				/>
-				<Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-				<Form.Control.Feedback type="invalid">
-					Please add your message.
-				</Form.Control.Feedback>
-			</Form.Group>
+				<Form.Group controlId="formEmail">
+					<Form.Label>Email Address</Form.Label>
+					<InputGroup>
+						<Form.Control
+							name="email"
+							type="email"
+							placeholder="Enter email"
+							required
+							onChange={updateInput}
+							value={formData.email || ""}
+						/>
 
-			<Form.Group>
-				<Form.Check
-					name="check"
-					required
-					// label={
-					// 	<Link
-					// 		className="text-warning"
-					// 		href="Terms-and-conditions"
-					// 	>
-					// 		I have read and agree to the Terms and Conditions
-					// 	</Link>
-					// }
-					label="I have read and agreed to the Terms and Conditions"
-					feedback="You must agree before submitting."
-				/>
-			</Form.Group>
-			<Button className="btn btn-dark my-2 my-md-2" type="submit">
-				{loading && (
-					<Spinner
-						as="span"
-						animation="border"
-						role="status"
-						aria-hidden="true"
+						<Form.Control.Feedback>
+							Looks good!
+						</Form.Control.Feedback>
+						<Form.Control.Feedback type="invalid">
+							Please enter a valid email.
+						</Form.Control.Feedback>
+					</InputGroup>
+					<Form.Text className="text-muted">
+						We'll never share your email with anyone else.
+					</Form.Text>
+				</Form.Group>
+
+				<Form.Group controlId="contactForm.Textarea">
+					<Form.Label>Message</Form.Label>
+					<Form.Control
+						as="textarea"
+						rows={3}
+						placeholder="Please enter your message"
+						required
+						name="message"
+						onChange={updateInput}
+						value={formData.message || ""}
 					/>
-				)}
+					<Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+					<Form.Control.Feedback type="invalid">
+						Please add your message.
+					</Form.Control.Feedback>
+				</Form.Group>
 
-				{!loading && "Submit"}
-			</Button>
-		</Form>
+				<button
+					className="btn btn-dark my-2 my-md-2"
+					type="submit"
+					value="Submit"
+				>
+					{loading && (
+						<Spinner
+							as="span"
+							animation="border"
+							role="status"
+							aria-hidden="true"
+						/>
+					)}
+
+					{!loading && "Submit"}
+				</button>
+			</Form>
+
+			<Toaster position="top-center" reverseOrder={false} />
+		</>
 	);
 }
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,28 +1,34 @@
-export default function handler(req, res) {
-	require("dotenv").config();
+import React from "react";
 
-	let nodemailer = require("nodemailer");
-	const transporter = nodemailer.createTransport({
-		port: 465,
-		host: "smtp.gmail.com",
-		secure: true,
-		auth: {
-			user: process.env.EMAIL,
-			pass: process.env.PASSWORD,
-		},
-	});
+export default async function handler(req, res) {
+	if (req.method !== "POST") {
+		return res.status(404).send({ error: "Not a POST method." });
+	}
+	try {
+		require("dotenv").config();
 
-	const mailData = {
-		from: process.env.EMAIL,
-		to: "plantbassddjs@gmail.com",
-		subject: `PlantBassd.com: Message From ${req.body.name}`,
-		text: `From ${req.body.email},\n\n${req.body.message}\n\n${req.body.name}`,
-	};
+		let nodemailer = require("nodemailer");
+		const transporter = nodemailer.createTransport({
+			port: 465,
+			host: "smtp.gmail.com",
+			secure: true,
+			auth: {
+				user: process.env.EMAIL,
+				pass: process.env.PASSWORD,
+			},
+		});
 
-	transporter.sendMail(mailData, function (err, info) {
-		if (err) console.log(err);
-		else console.log(info);
-	});
+		const mailData = {
+			from: process.env.EMAIL,
+			to: process.env.INBOX,
+			subject: `PlantBassd.com: Message From ${req.body.name}`,
+			text: req.body.message + " | Sent from: " + req.body.email,
+			html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`,
+		};
 
-	res.status(200);
+		transporter.sendMail(mailData);
+		res.status(200).json({ message: "message sent" });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
 }
