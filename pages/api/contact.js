20a03c19389547a429140d122040365a5ab2e3a9
savagem7 <i0g1n4oj>
2021-12-21 22:04:24 +0000

    remove dotenv form contact form

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,12 +1,8 @@
-import React from "react";
-
 export default async function handler(req, res) {
 	if (req.method !== "POST") {
 		return res.status(404).send({ error: "Not a POST method." });
 	}
 	try {
-		require("dotenv").config();
-
 		let nodemailer = require("nodemailer");
 		const transporter = nodemailer.createTransport({
 			port: 465,
