e4211476519c4297d5b3fcd4464de8f3ac4ce576
savagem7 <i0g1n4oj>
2022-01-07 22:47:10 +0000

    debugging contact form not working

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,33 +1,37 @@
+let nodemailer = require("nodemailer");
+
 export default async function handler(req, res) {
 	if (req.method !== "POST") {
 		return res.status(404).send({ error: "Not a POST method." });
 	}
-	try {
-		let nodemailer = require("nodemailer");
-		const transporter = nodemailer.createTransport({
-			port: 465,
-			host: "smtp.gmail.com",
-			secure: true,
-			auth: {
-				user: process.env.EMAIL,
-				pass: process.env.PASSWORD,
-			},
-		});
+	const transporter = nodemailer.createTransport({
+		port: 465,
+		host: "smtp.gmail.com",
+		secure: true,
+		auth: {
+			user: process.env.EMAIL,
+			pass: process.env.PASSWORD,
+		},
+	});
 
-		const mailData = {
-			from: process.env.EMAIL,
-			to: process.env.INBOX,
-			subject: `PlantBassd.com: Message From ${req.body.name}`,
-			text: req.body.message + " | Sent from: " + req.body.email,
-			html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`,
-		};
+	const mailData = {
+		from: process.env.EMAIL,
+		to: process.env.INBOX,
+		subject: `PlantBassd.com: Message From ${req.body.name}`,
+		text: req.body.message + " | Sent from: " + req.body.email,
+		html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`,
+	};
 
-		transporter.sendMail(mailData);
-		res.status(200).json({
-			message: "Successfully sent",
-			receiver: process.env.INBOX,
-		});
-	} catch (err) {
-		res.status(500).json({ error: err.message });
-	}
+	transporter.sendMail(mailData, (err, data) => {
+		if (err) {
+			console.log(err);
+			res.send("error" + JSON.stringify(err));
+		} else {
+			console.log("mail send");
+			res.status(200).json({
+				message: "Successfully sent",
+				receiver: process.env.INBOX,
+			});
+		}
+	});
 }
