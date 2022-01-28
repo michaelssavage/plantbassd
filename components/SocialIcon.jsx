7c6011c0960d657113b8008cc337edb6b06ee345
savagem7 <i0g1n4oj>
2022-01-28 11:41:44 +0000

    refactor eslint, prettier, and packages

--- components/SocialIcon.jsx
+++ components/SocialIcon.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React from "react";
+import { useMemo } from "react";
 import { IconContext } from "react-icons";
 import {
 	AiFillHome,
@@ -14,39 +14,40 @@ import { ImNewspaper } from "react-icons/im";
 import { MdContacts } from "react-icons/md";
 import { RiSoundcloudLine, RiSpotifyLine } from "react-icons/ri";
 
-export default function SocialIcon({ icon, styling }) {
-	const Switcher = () => {
-		switch (icon) {
-			case "Home":
-				return <AiFillHome />;
-			case "News":
-				return <ImNewspaper />;
-			case "Mixes":
-				return <RiSoundcloudLine />;
-			case "Takeovers":
-				return <FaSpotify />;
-			case "Radio":
-				return <BiRadio />;
-			case "Contact Us":
-				return <MdContacts />;
-			case "Links":
-				return <AiOutlineLink />;
-			case "facebook":
-				return <AiOutlineFacebook />;
-			case "instagram":
-				return <AiOutlineInstagram />;
-			case "email":
-				return <AiOutlineMail />;
-			case "soundcloud":
-				return <RiSoundcloudLine />;
-			default:
-				return <RiSpotifyLine />;
-		}
-	};
+function Switcher({ icon }) {
+	switch (icon) {
+		case "Home":
+			return <AiFillHome />;
+		case "News":
+			return <ImNewspaper />;
+		case "Mixes":
+			return <RiSoundcloudLine />;
+		case "Takeovers":
+			return <FaSpotify />;
+		case "Radio":
+			return <BiRadio />;
+		case "Contact Us":
+			return <MdContacts />;
+		case "Links":
+			return <AiOutlineLink />;
+		case "facebook":
+			return <AiOutlineFacebook />;
+		case "instagram":
+			return <AiOutlineInstagram />;
+		case "email":
+			return <AiOutlineMail />;
+		case "soundcloud":
+			return <RiSoundcloudLine />;
+		default:
+			return <RiSpotifyLine />;
+	}
+}
 
+export default function SocialIcon({ icon, styling }) {
+	const styleMemo = useMemo(() => ({ className: styling }), [styling]);
 	return (
-		<IconContext.Provider value={{ className: styling }}>
-			<Switcher />
+		<IconContext.Provider value={styleMemo}>
+			<Switcher icon={icon} />
 		</IconContext.Provider>
 	);
 }
@@ -54,4 +55,8 @@ export default function SocialIcon({ icon, styling }) {
 SocialIcon.propTypes = {
 	icon: PropTypes.string.isRequired,
 	styling: PropTypes.string.isRequired,
+};
+
+Switcher.propTypes = {
+	icon: PropTypes.string.isRequired,
 };
