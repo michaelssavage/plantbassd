0e4b196343987c7ffa5c2c3b594d802fbdf49bf3
savagem7 <i0g1n4oj>
2022-02-03 14:24:01 +0000

    linktree update

--- arrays/linktree.jsx
+++ arrays/linktree.jsx
@@ -1,11 +1,19 @@
 export const linkList = [
 	{
-		title: "FRESH JUICE: Long Island Sound - First Contact",
-		link: "https://www.plantbassd.com/news/long-island-sound-31-1-22",
+		title: "Plant Bass'd Radio: Sweet Philly Raunchy Mix",
+		link: "https://soundcloud.com/plantbassddjs/plant-bassd-radio-sweet-philly-raunchy-mix",
 	},
 	{
-		title: "EDINBURGH WEEKLY CLUB GUIDE: REOPENING WEEK",
-		link: "https://www.plantbassd.com/news/edi-weekly-club-guide-24-1-22",
+		title: "TAAHLIAH @ Sneaky Pete's Tickets",
+		link: "https://ra.co/events/1494126",
+	},
+	{
+		title: "EDINBURGH WEEKLY CLUB GUIDE",
+		link: "https://www.plantbassd.com/news/edi-weekly-club-guide-1-2-22",
+	},
+	{
+		title: "FRESH JUICE: Long Island Sound - First Contact",
+		link: "https://www.plantbassd.com/news/long-island-sound-31-1-22",
 	},
 	{
 		title: "FRESH JUICE: CLAN DESTINE - 'THEE ALPHA AND THEE OMEGA VOL. IX'",
@@ -16,20 +24,12 @@ export const linkList = [
 		link: "https://www.plantbassd.com/news/jason-code-14-1-22",
 	},
 	{
-		title: "Tickets for TAAHLIAH @ Sneaky Pete's 9/2/22",
-		link: "https://ra.co/events/1494126",
-	},
-	{
 		title: "FRESH JUICE: Third Degree - 'HAZMAT Compilation'",
 		link: "https://www.plantbassd.com/news/third-degree-10-1-22",
 	},
 	{
 		title: "FRESH JUICE: Oldboy - GRIMEY ONES PART II",
 		link: "https://www.plantbassd.com/news/oldboy-7-1-22",
-	},
-	{
-		title: "Plant Bass'd Radio: Saz Powerhouse Mix",
-		link: "https://soundcloud.com/plantbassddjs/plant-bassd-radio-saz-mix",
 	},
 	{
 		title: "Plant Bass'd Picks: Rory Sweeney",
