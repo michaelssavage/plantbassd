e729f5636f51b1acac9532ab1b682a812eeca37b
Michael S <33690140+michaelssavage@users.noreply.github.com>
2022-02-04 18:39:10 +0000

    Update linktree.jsx

--- arrays/linktree.jsx
+++ arrays/linktree.jsx
@@ -17,26 +17,26 @@ export const linkList = [
 	},
 	{
 		title: "FRESH JUICE: Long Island Sound - First Contact",
-		link: "https://www.plantbassd.com/news/long-island-sound-31-1-22",
+		link: "https://www.plantbassd.com/fresh-juice/long-island-sound-31-1-22",
 	},
 	{
 		title: "FRESH JUICE: CLAN DESTINE - 'THEE ALPHA AND THEE OMEGA VOL. IX'",
-		link: "https://www.plantbassd.com/news/clan-destine-21-1-22",
+		link: "https://www.plantbassd.com/fresh-juice/clan-destine-21-1-22",
 	},
 	{
 		title: "FRESH JUICE: Jason Code - 'JCO1'",
-		link: "https://www.plantbassd.com/news/jason-code-14-1-22",
+		link: "https://www.plantbassd.com/fresh-juice/jason-code-14-1-22",
 	},
 	{
 		title: "FRESH JUICE: Third Degree - 'HAZMAT Compilation'",
-		link: "https://www.plantbassd.com/news/third-degree-10-1-22",
+		link: "https://www.plantbassd.com/fresh-juice/third-degree-10-1-22",
 	},
 	{
 		title: "FRESH JUICE: Oldboy - GRIMEY ONES PART II",
-		link: "https://www.plantbassd.com/news/oldboy-7-1-22",
+		link: "https://www.plantbassd.com/fresh-juice/oldboy-7-1-22",
 	},
 	{
-		title: "Plant Bass'd Picks: Rory Sweeney",
+		title: "Spotify Playlist: Plant Bass'd Picks",
 		link: "https://open.spotify.com/playlist/5skAgzUfGmZLwrOPNLnGVf?si=611353a4ea0244d5",
 	},
 	{
