a6804f38f4eaa41fb9f2cedd657589036e980f0e
savagem7 <i0g1n4oj>
2022-11-04 14:54:18 +0000

    add audience analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
 import "../styles/styles.global.scss"; // eslint-disable-line
 import "bootstrap/dist/css/bootstrap.min.css"; // eslint-disable-line
-
+import { Analytics } from "@vercel/analytics/react";
 import Head from "next/head";
 import PropTypes from "prop-types";
 import Sidebar from "components/Sidebar";
@@ -12,6 +12,7 @@ export default function MyApp({ Component, pageProps }) {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       <Sidebar />
+      <Analytics />
       <Component {...pageProps} />
     </>
   );
