ebd64e3764ad865b5648bce0a56d09a4f1ceace8
savagem7 <i0g1n4oj>
2023-01-25 00:13:07 +0000

    update team page

--- arrays/our-team.ts
+++ arrays/our-team.ts
@@ -16,12 +16,6 @@ export const ourTeam: TeamProps[] = [
     img: "michael.jpg",
     link: "https://www.instagram.com/michaelsaverage/",
     name: "Michael Savage",
-    role: "Graphics/ Website Design",
-  },
-  {
-    img: "peter.jpg",
-    link: "https://www.instagram.com/peter_toal/",
-    name: "Peter Toal",
-    role: "Communications",
+    role: "Graphics/ Website",
   },
 ];
