f4345162d69d72b7c45e6e47de4e9400a54892d0
savagem7 <i0g1n4oj>
2023-02-01 15:04:51 +0000

    january under the radar picks

--- components/Slug/Content.tsx
+++ components/Slug/Content.tsx
@@ -13,12 +13,12 @@ interface ContentProps {
 export const Content = ({ title, pic, link, description, standout, tags }: ContentProps) => {
   return (
     <>
-      <h2>{title}</h2>
       <div className="row display-flex align-items-center">
         <div className="col clubGuide">
           <Picture src={pic} alt={title} size={1200} />
         </div>
         <div className="col clubGuide">
+          <h2>{title}</h2>
           <div>
             {description}
             <br />
