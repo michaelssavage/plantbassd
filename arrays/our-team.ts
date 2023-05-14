1dd5c9f27cd88b0c6bdcaf53a9bfe853628d5e4f
savagem7 <i0g1n4oj>
2023-05-14 23:48:57 +0100

    update team page and styling

--- arrays/our-team.ts
+++ arrays/our-team.ts
@@ -2,7 +2,6 @@ interface TeamProps {
   img: string;
   link: string;
   name: string;
-  role: string;
 }
 
 export const ourTeam: TeamProps[] = [
@@ -10,12 +9,15 @@ export const ourTeam: TeamProps[] = [
     img: "08-oisin.jpg",
     link: "https://www.instagram.com/inspectorcampbellbap/",
     name: "Oisin Campbell",
-    role: "Director",
   },
   {
     img: "michael.jpg",
     link: "https://www.instagram.com/michaelsaverage/",
     name: "Michael Savage",
-    role: "Graphics/ Website",
+  },
+  {
+    img: "peter.jpg",
+    link: "https://www.instagram.com/peter_toal/",
+    name: "Peter Toal",
   },
 ];
