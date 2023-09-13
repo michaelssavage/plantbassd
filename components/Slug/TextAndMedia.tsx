2c99bd152a61f83aece2c5885ed7d99eda34a735
savagem7 <i0g1n4oj>
2023-09-13 18:44:32 +0100

    premiere and fuinneamh writep

--- components/Slug/TextAndMedia.tsx
+++ components/Slug/TextAndMedia.tsx
@@ -7,13 +7,14 @@ interface FavTrackProps {
   children: ReactElement;
   soundcloud?: string;
   bandcamp?: string;
+  youtube?: string;
 }
 
 /**
  * TextAndMedia is used in the news all together now
  */
 export const TextAndMedia = (props: FavTrackProps) => {
-  const { title, children, soundcloud, bandcamp } = props;
+  const { title, children, soundcloud, bandcamp, youtube } = props;
 
   return (
     <div className="mb-5">
@@ -23,7 +24,8 @@ export const TextAndMedia = (props: FavTrackProps) => {
           <div className={styles.childrenContainer}>{children}</div>
         </div>
         <div className="col-md-7 col-sm-12">
-          {soundcloud && <SoundCloud url={soundcloud} height="120px" />}
+          {soundcloud && <SoundCloud url={soundcloud} height="140px" />}
+          {youtube && <SoundCloud url={youtube} height="240px" />}
           {bandcamp && <BandCamp src={bandcamp} top />}
         </div>
       </div>
