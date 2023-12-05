9a3409eaba0d718473e49991c393f190f863bc7d
savagem7 <i0g1n4oj>
2023-12-05 13:49:12 +0000

    image metadata

--- components/PageMetaData.tsx
+++ components/PageMetaData.tsx
@@ -12,14 +12,15 @@ export default function PageMetaData({ title, description, imageUrl, url }: Titl
     <Head>
       <title>{title}</title>
       <meta property="og:title" content={description} />
-      {description && (
-        <>
-          <meta name="description" content={description} />
-          <meta name="og:description" property="og:description" content={description} />
-        </>
-      )}
+      {description && <meta name="description" content={description} />}
+      {description && <meta name="og:description" content={description} />}
       {imageUrl && <meta property="og:url" content={`${url}`} />}
-      {url && <meta name="og:image" property="og:image" content={imageUrl} />}
+      {url && (
+        <meta
+          name="og:image"
+          content={`https://res.cloudinary.com/ddscru1gt/image/upload/c_limit,w_600/f_auto/q_auto/v1/${imageUrl}`}
+        />
+      )}
     </Head>
   );
 }
