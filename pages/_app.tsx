c0bc44415634dd1fafae4f2da73ff289e126ab64
savagem7 <i0g1n4oj>
2023-12-08 13:02:19 +0000

    jwy post

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/styles.global.scss"; // eslint-disable-line
 import "bootstrap/dist/css/bootstrap.min.css"; // eslint-disable-line
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import { ErrorBoundary } from "react-error-boundary";
@@ -36,6 +37,7 @@ export default function MyApp({ Component, pageProps, ...appProps }: AppProps) {
       </Head>
       <Sidebar />
       <Analytics />
+      <SpeedInsights />
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         {loading ? <Loading /> : <Component {...pageProps} />}
       </ErrorBoundary>
